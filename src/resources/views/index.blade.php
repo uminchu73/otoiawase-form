03015059ce64edb46598a9a1394cd5a5b802e21d
uminchu <ichigoichie.423915@gmail.com>
2025-07-03 15:48:24 +0900

    indexビュー⚪︎

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,173 @@
+{{-- レイアウトファイルの読み込み --}}
+@extends('layouts/app')
+
+    @section('css')
+        {{-- index.cssの読み込み --}}
+        <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+
+        <!-- フォントの読み込み -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inika:wght@400;700&family=Noto+Serif+JP:wght@900&display=swap" rel="stylesheet">
+    @endsection
+
+@section('content')
+
+{{-- 見出し --}}
+    <div class="contact-box">
+        <div class="contact-title">
+            <a>Contact</a>
+        </div>
+        {{-- お問い合わせフォーム --}}
+        <form class="contact-form" action="/contacts/confirm" method="post">
+            @csrf
+            {{-- 名前の入力欄 --}}
+            <div class="contact-form__item">
+                <div class="contact-form__item--label-box">
+                    <span class="contact-form__item--label">お名前</span>
+                    <span class="contact-form__item--label-required">※</span>
+                </div>
+                <div class="contact-form__item--input-box">
+                    {{-- 入力欄 --}}
+                    <input type="text" name="first-name" class="contact-form__item--input" value="{{ old('first-name') }}" placeholder="例：山田" />
+                    <input type="text" name="last-name" class="contact-form__item--input" value="{{ old('last-name') }}" placeholder="例：太朗" />
+                    {{-- エラー表示 --}}
+                    <div class="error">
+                        @error('name')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            {{-- 性別の入力欄 --}}
+            <div class="contact-form__item">
+                <div class="contact-form__item--label-box">
+                    <span class="contact-form__item--label">性別</span>
+                    <span class="contact-form__item--label-required">※</span>
+                </div>
+                <div class="contact-form__item--input-box">
+                    {{-- ラジオボタン --}}
+                    <label><input type="radio" name="gender" value="男性" {{ old('gender', '男性') == '男性' ? 'checked' : '' }}>男性</label>
+                    <label><input type="radio" name="gender" value="女性" {{ old('gender') == '女性' ? 'checked' : '' }}>女性</label>
+                    <label><input type="radio" name="gender" value="その他" {{ old('gender') == 'その他' ? 'checked' : '' }}>その他</label>
+                    {{-- エラー表示 --}}
+                    <div class="error">
+                        @error('gender')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            {{-- メールアドレス入力欄 --}}
+            <div class="contact-form__item">
+                <div class="contact-form__item--label-box">
+                    <span class="contact-form__item--label">メールアドレス</span>
+                    <span class="contact-form__item--label-required">※</span>
+                </div>
+                <div class="contact-form__item--input-box">
+                    {{-- 入力欄 --}}
+                    <input type="email" name="email" class="contact-form__item--input" value="{{ old('email') }}" placeholder="例：test@example.com" />
+                    {{-- エラー表示 --}}
+                    <div class="error">
+                        @error('email')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            {{-- 電話番号入力欄 --}}
+            <div class="contact-form__item">
+            <div class="contact-form__item--label-box">
+                    <span class="contact-form__item--label">電話番号</span>
+                    <span class="contact-form__item--label-required">※</span>
+                </div>
+                <div class="contact-form__item--input-box">
+                    {{-- 入力欄 --}}
+                    <input type="tel" name="tel-1" class="contact-form__item--input" value="{{ old('tel-1') }}" placeholder="080" />
+                    <span class="contact-form__item--label">-</span>
+                    <input type="tel" name="tel-2" class="contact-form__item--input" value="{{ old('tel-2') }}" placeholder="1234" />
+                    <span class="contact-form__item--label">-</span>
+                    <input type="tel" name="tel-3" class="contact-form__item--input" value="{{ old('tel-3') }}" placeholder="5678" />
+                    {{-- エラー表示 --}}
+                    <div class="error">
+                        @error('tel')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            {{-- 住所の入力欄 --}}
+            <div class="contact-form__item">
+                <div class="contact-form__item--label-box">
+                    <span class="contact-form__item--label">住所</span>
+                    <span class="contact-form__item--label-required">※</span>
+                </div>
+                <div class="contact-form__item--input-box">
+                    {{-- 入力欄 --}}
+                    <input type="text" name="address" class="contact-form__item--input" value="{{ old('address') }}" placeholder="例：東京都渋谷区千駄ヶ谷1-2-3" />
+                    {{-- エラー表示 --}}
+                    <div class="error">
+                        @error('address')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            {{-- 建物名の入力欄 --}}
+            <div class="contact-form__item">
+                <div class="contact-form__item--label-box">
+                    <span class="contact-form__item--label">建物名</span>
+                </div>
+                <div class="contact-form__item--input-box">
+                    {{-- 入力欄 --}}
+                    <input type="text" name="bldg" class="contact-form__item--input" value="{{ old('bldg') }}" placeholder="例：千駄ヶ谷マンション101" />
+                </div>
+            </div>
+            {{-- お問い合わせの種類選択欄 --}}
+            <div class="contact-form__item">
+                <div class="contact-form__item--label-box">
+                    <span class="contact-form__item--label">お問い合わせの種類</span>
+                    <span class="contact-form__item--label-required">※</span>
+                </div>
+                <div class="contact-form__item--input-box">
+                    {{-- 選択欄 --}}
+                    <select name="content-select" id="" class="contact-form__item--input">
+                        <option value="" disabled {{ old('content-select') == '' ? 'selected' : '' }}>選択してください</option>
+                        <option value="商品のお届けについて" {{ old('content-select') == '商品のお届けについて' ? 'selected' : '' }}>商品のお届けについて</option>
+                        <option value="商品の交換について" {{ old('content-select') == '商品の交換について' ? 'selected' : '' }}>商品の交換について</option>
+                        <option value="商品トラブル" {{ old('content-select') == '商品トラブル' ? 'selected' : '' }}>商品トラブル</option>
+                        <option value="ショップへのお問い合わせ" {{ old('content-select') == 'ショップへのお問い合わせ' ? 'selected' : '' }}>ショップへのお問い合わせ</option>
+                        <option value="その他" {{ old('content-select') == 'その他' ? 'selected' : '' }}>その他</option>
+                    </select>
+                    {{-- エラー表示 --}}
+                    <div class="error">
+                        @error('content-select')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            {{-- お問い合わせ内容入力欄 --}}
+            <div class="contact-form__item">
+                <div class="contact-form__item--label-box">
+                    <span class="contact-form__item--label">お問合せ内容</span>
+                    <span class="contact-form__item--label-required">※</span>
+                </div>
+                <div class="contact-form__item--input-box">
+                    {{-- 入力欄 --}}
+                    <textarea name="content" class="contact-form__item--textarea" placeholder="お問い合わせ内容をご記載ください">{{ old('content') }}</textarea>
+                    {{-- エラー表示 --}}
+                    <div class="error">
+                        @error('content')
+                        {{ $message }}
+                        @enderror
+                    </div>
+                </div>
+            </div>
+            {{-- 送信ボタン --}}
+            <div class="contact-form__btn">
+                <button class="contact-form__btn-submit" type="submit">確認画面</button>
+            </div>
+        </form>
+    </div>
+@endsection
---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!-- 共通レイアウトのHTML -->
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <!-- 文字コードやレスポンシブ対応 -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- ページタイトル -->
+    <title>FashionablyLate</title>
+
+    <!-- 共通CSSの読み込み -->
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+    <!-- フォントの読み込み -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inika:wght@400;700&family=Noto+Serif+JP:wght@900&display=swap" rel="stylesheet">
+
+    <!-- 各ページごとの追加CSS -->
+    @yield('css')
+</head>
+
+<body>
+    <!-- ヘッダー -->
+    <header class="header">
+        <div class="header__inner">
+                <a class="header__logo" href="/">
+                FashionablyLate
+                </a>
+            </div>
+        </div>
+    </header>
+
+    <!-- ページごとの中身を表示 -->
+    <main>
+        @yield('content')
+    </main>
+</body>
+
+</html>
